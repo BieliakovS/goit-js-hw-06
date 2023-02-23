3130fd5bea4da645e63264653227d3ef789a1095
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-02-24 01:04:31 +0200

    task 4 correct

--- js/task-04.js
+++ js/task-04.js
@@ -4,9 +4,9 @@ const valueEl = document.querySelector("#value");
 let counterValue = 0;
 
 counterEl.firstElementChild.addEventListener("click", () => {
-  valueEl.textContent = `${(counterValue += 1)}`;
+  valueEl.textContent = `${(counterValue -= 1)}`;
 });
 
 counterEl.lastElementChild.addEventListener("click", () => {
-  valueEl.textContent = `${(counterValue -= 1)}`;
+  valueEl.textContent = `${(counterValue += 1)}`;
 });
