85563a39ebe58f349db35b94d81a20ece0e434d3
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-02-24 01:01:34 +0200

    3, 4 tasks

---
+++ js/task-04.js
@@ -0,0 +1,12 @@
+const counterEl = document.querySelector("#counter");
+const valueEl = document.querySelector("#value");
+
+let counterValue = 0;
+
+counterEl.firstElementChild.addEventListener("click", () => {
+  valueEl.textContent = `${(counterValue += 1)}`;
+});
+
+counterEl.lastElementChild.addEventListener("click", () => {
+  valueEl.textContent = `${(counterValue -= 1)}`;
+});
