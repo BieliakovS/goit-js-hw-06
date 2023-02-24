b3ffd921c0127ff18a41d0440a9ce4afee852ba7
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-02-25 01:57:33 +0200

    9 task

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+const handleChangeColorSubmit = (event) => {
+  const getCoor = getRandomHexColor();
+  colorEl.textContent = `${getCoor}`;
+  bodyEl.style.backgroundColor = `${getCoor}`;
+};
+
+changeColorEl.addEventListener("click", handleChangeColorSubmit);
