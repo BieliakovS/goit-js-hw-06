082b2f5b2803e70b74907cdc23b2da52236aea13
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-02-25 14:50:34 +0200

    10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsEl = document.querySelector("#controls");
+const createBtnEl = document.querySelector("button[data-create]");
+const destroyBtnEl = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const divArray = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    divArray.push(box);
+  }
+
+  boxesEl.append(...divArray);
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
+
+createBtnEl.addEventListener("click", (event) => {
+  const amount = Number(controlsEl.querySelector("input").value);
+  createBoxes(amount);
+});
+
+destroyBtnEl.addEventListener("click", (event) => {
+  destroyBoxes();
+});
