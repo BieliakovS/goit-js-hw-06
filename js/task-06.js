48bcdd296bfb70223faa250702249f6593dd1c22
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-02-26 22:49:31 +0200

    6 task correct

--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,12 @@ const inputEl = document.querySelector("#validation-input");
 const validSymbolsLength = Number(inputEl.getAttribute("data-length"));
 
 const handleSymbolsValid = (event) => {
-  if (event.target.value.length !== validSymbolsLength) {
-    inputEl.classList.replace("valid", "invalid");
-    inputEl.classList.remove("valid");
-  } else {
+  if (event.target.value.length === validSymbolsLength) {
     inputEl.classList.add("valid");
     inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
   }
 };
 
